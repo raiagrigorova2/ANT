c99e6a640e77e8299a76c389b70a9be39923799e
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-10-12 13:46:51 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
-#include "CVector.h"
+
 using namespace std;
 
-
+#include "CVector.h"
+#include "CRecipe.h"
 int main()
 {
-    CVector vec;
-    for(char c = 'a'; c <= 'z'; c++){
-        vec.push_back(c);
-        vec.print();
+    CVector <char> vec;
+    CVector <int> vec2;
 
+    CVector <CRecipe> vec3;
+    vec3.push_back(CRecipe("Palachinki",1));
+
+//    for(char c = 'a'; c <= 'z'; c++){
+//        vec.push_back(c);
+//        vec.print();
+//
+//    }
+//    for(int i = 0; i < 33; i += 3){
+//        vec.insert('#', i);
+//        vec.print();
+//    }
+
+    for(int i = 0; i < 10 ; i++){
+        vec2.push_back(i);
+        vec2.print();
     }
-    for(int i = 0; i < 33; i += 3){
-        vec.insert('#', i);
-        vec.print();
+
+    for(int i = 0; i < 10 ; i+=4){
+        vec2.insert(99,i);
+        vec2.print();
     }
+
+    CRecipe res;
+    cin>>res;
+
+    vec3.push_back(res);
+    vec3.print();
 
     return 0;
 }
